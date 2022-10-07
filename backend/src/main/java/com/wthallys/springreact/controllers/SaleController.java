6ecc0ab0d6b473f4282eb37d8b6f89aec28648b9
wthallys <wthallys.02@gmail.com>
2022-10-07 00:31:52 -0300

    Twilio SMS added

--- backend/src/main/java/com/wthallys/springreact/controllers/SaleController.java
+++ backend/src/main/java/com/wthallys/springreact/controllers/SaleController.java
@@ -3,13 +3,11 @@ package com.wthallys.springreact.controllers;
 
 import com.wthallys.springreact.entities.Sale;
 import com.wthallys.springreact.services.SaleService;
+import com.wthallys.springreact.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,11 +18,19 @@ public class SaleController {
     @Autowired
     private SaleService service;
 
+    @Autowired
+    private SmsService smsService;
+
     @GetMapping
     public Page<Sale> findSales(
             @RequestParam(value = "minimumDate", defaultValue = "") String minimumDate,
             @RequestParam(value = "maximumDate", defaultValue = "") String maximumDate,
             Pageable pageable) {
         return service.findSales(minimumDate, maximumDate, pageable);
+    }
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
     }
 }
